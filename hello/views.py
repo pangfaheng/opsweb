6b4cf05b33c1812f1f1c3eab48cf22721e2bd16a
pangfaheng <pangfaheng@163.com>
2024-04-18 10:34:52 +0800

    1

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
+from django.shortcuts import render
 
 
 def home(request):
@@ -9,17 +10,7 @@ def home(request):
 
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+    print(request.build_absolute_uri())  # optional
+    return render(
+        request, "hello/hello_there.html", {"name": name, "date": datetime.now()}
+    )
