8bb5ad4ca46846256c703a25350235b413320ebc
pangfaheng <pangfaheng@163.com>
2024-04-18 11:29:51 +0800

    111

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from hello import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("hello/<name>", views.hello_there, name="hello_there"),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
