3d7ff5abadc63f7a550ede3b1bba0055a92e3fcf
pangfaheng <pangfaheng@163.com>
2024-04-29 14:58:23 +0800

    1

--- hello/urls.py
+++ hello/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("hello/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
 from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 
 def home(request):
@@ -22,3 +26,16 @@ def about(request):
 
 def contact(request):
     return render(request, "hello/contact.html")
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
