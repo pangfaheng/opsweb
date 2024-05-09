bfb3a7919b31963ae2c56f7505d875547793d9f8
pangfaheng <pangfaheng@163.com>
2024-05-09 17:33:37 +0800

    1

--- sysadmin/views.py
+++ sysadmin/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from sysadmin.models import instance_base_info
 
 def sysadmin_home(request):
     return render(request, "sysadmin/home.html")
 
 def sysadmin_server_assets(request):
-    return render(request, "sysadmin/sysadmin_server_assets.html")
+    data = instance_base_info.objects.all()
+    ppa = 'hahaha'
+    return render(request, "sysadmin/sysadmin_server_assets.html", {'data': data, 'ppa': ppa})
