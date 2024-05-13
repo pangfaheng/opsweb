07885911687d93df9a6cc4a92dabf3bf8d9431e1
pangfaheng <pangfaheng@163.com>
2024-05-13 17:43:33 +0800

    1

--- sysadmin/views.py
+++ sysadmin/views.py
@@ -4,6 +4,9 @@ from sysadmin.models import instance_base_info
 def sysadmin_home(request):
     return render(request, "sysadmin/home.html")
 
+def __sysadmin_server_assets_total(data):
+    return len(data)
+
 def sysadmin_server_assets(request):
     data = instance_base_info.objects.all()
-    return render(request, "sysadmin/sysadmin_server_assets.html", {'data': data,})
+    return render(request, "sysadmin/sysadmin_server_assets.html", {'data': data, 'server_total': __sysadmin_server_assets_total(data = data)})
