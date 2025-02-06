ce22c3c727af831e4dc443d77c2e0809c1d2feb7
pangfaheng <pangfaheng@163.com>
2025-02-06 09:23:24 +0800

    1

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+
+from .models import Category, SubCategory
+
+
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    extra = 1  # 默认添加一条空白记录
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [SubCategoryInline]  # 在 Category 页面下显示 SubCategory
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory)
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
+from .models import Category
 
 
 @login_required
 def home(request):
-    return render(request, "home/index.html")
+    categories = Category.objects.all()
+    return render(request, "home/index.html", {"categories": categories})
